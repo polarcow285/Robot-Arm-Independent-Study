f80ebc6fad01b5b1128e0e01fdae49b019711ef9
Natalie Leung <40549274+polarcow285@users.noreply.github.com>
2023-04-04 09:34:37 -0700

    inverse kinematics finding nearest point

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ def on_forever():
     basic.pause(1000)
 #basic.forever(on_forever)
 #thetas = inv_solve(23.5, 0, 19)
-thetas = inv_solve(21.5, 0, 19)
+#thetas = inv_solve(21.5, 0, 19)
+moveToPos(15, 0 , 12)
+serial.write_line("done")
+#thetas = inv_solve(15, 0, 12)
 #moveTo(thetas[0][0], thetas[0][1], thetas[0][2])
 
 def moveTo(target1, target2, target3):
@@ -44,6 +47,19 @@ def moveTo(target1, target2, target3):
         hummingbird.set_position_servo(FourPort.TWO, link2CurrPos)
         hummingbird.set_position_servo(FourPort.THREE, link3CurrPos)
         basic.pause(10)
+def moveToPos(x, y, z):
+    thetas = inv_solve(x,y,z)
+    tolerance = 12
+    #serial.write_value(len)
+    if(len(thetas)==0):
+        serial.write_line("inside while loop")
+        for a in range(tolerance+1):
+            for b in range(tolerance+1):
+                for c in range(tolerance+1):
+                    serial.write_line("doing inv_solve")
+                    thetas = inv_solve(x+a,y+b,z+c)
+                    thetas = inv_solve(x-a,y-b,z-c)
+    return thetas[0]        
 
 def inv_solve(x, y, z):
     AP = Math.sqrt(x**2 + y**2)
@@ -62,6 +78,7 @@ def inv_solve(x, y, z):
     theta1_2 = theta1_1 + Math.PI
 
     #convert to degrees
+    """
     theta1_1 = theta1_1 * (180/Math.PI)
     theta1_2 = theta1_2 * (180/Math.PI)
 
@@ -72,17 +89,17 @@ def inv_solve(x, y, z):
     theta2_2 = theta2_2 * (180/Math.PI)
     theta2_3 = theta2_3 * (180/Math.PI)
     theta2_4 = theta2_4 * (180/Math.PI)
+    """
+    theta1_1 = convertToDegrees(theta1_1)
+    theta1_2 = convertToDegrees(theta1_2)
     
-    theta1_1 = Math.round(theta1_1)
-    theta1_2 = Math.round(theta1_2)
+    theta2_1 = convertToDegrees(theta2_1)
+    theta2_2 = convertToDegrees(theta2_2)
+    theta2_3 = convertToDegrees(theta2_3)
+    theta2_4 = convertToDegrees(theta2_4)
     
-    theta2_1 = Math.round(theta2_1)
-    theta2_2 = Math.round(theta2_2)
-    theta2_3 = Math.round(theta2_3)
-    theta2_4 = Math.round(theta2_4)
-    
-    theta3_1 = Math.round(theta3_1)
-    theta3_2 = Math.round(theta3_2)
+    theta3_1 = convertToDegrees(theta3_1)
+    theta3_2 = convertToDegrees(theta3_2)
     
     set1 = (theta1_1, theta2_1, theta3_1)
     set2 = (theta1_1, theta2_2, theta3_2)
@@ -107,11 +124,20 @@ def inv_solve(x, y, z):
                 break
         if(isValid == True):
             res.append(_set)
-    
-    for _s in res:
-        serial.write_value("theta1", _s[0])
-        serial.write_value("theta2", _s[1])
-        serial.write_value("theta3", _s[2])
+    if(len(res) == 0):
+        serial.write_line("no values work :(")
+    else:
+        for _s in res:
+            serial.write_value("theta1", _s[0])
+            serial.write_value("theta2", _s[1])
+            serial.write_value("theta3", _s[2])
     
     return res;
-    
+def convertToDegrees(theta):
+    #checking for nan, nan is not equal to itself
+    if(theta != theta):
+        return Math.round(theta * (180/Math.PI))
+    else:
+        #print("Error: cannot perform operation with NaN value.")
+        #returns -1 because later we get rid of sets that have negative thetas
+        return -1    
--- main.ts
+++ main.ts
@@ -18,7 +18,10 @@ function on_forever() {
 
 // basic.forever(on_forever)
 // thetas = inv_solve(23.5, 0, 19)
-let thetas = inv_solve(21.5, 0, 19)
+// thetas = inv_solve(21.5, 0, 19)
+moveToPos(15, 0, 12)
+serial.writeLine("done")
+// thetas = inv_solve(15, 0, 12)
 // moveTo(thetas[0][0], thetas[0][1], thetas[0][2])
 function moveTo(target1: number, target2: number, target3: number) {
     target2 = 180 - target2
@@ -51,6 +54,26 @@ function moveTo(target1: number, target2: number, target3: number) {
     }
 }
 
+function moveToPos(x: number, y: number, z: number) {
+    let thetas = inv_solve(x, y, z)
+    let tolerance = 12
+    // serial.write_value(len)
+    if (thetas.length == 0) {
+        serial.writeLine("inside while loop")
+        for (let a = 0; a < tolerance + 1; a++) {
+            for (let b = 0; b < tolerance + 1; b++) {
+                for (let c = 0; c < tolerance + 1; c++) {
+                    serial.writeLine("doing inv_solve")
+                    thetas = inv_solve(x + a, y + b, z + c)
+                    thetas = inv_solve(x - a, y - b, z - c)
+                }
+            }
+        }
+    }
+    
+    return thetas[0]
+}
+
 function inv_solve(x: number, y: number, z: number): any[] {
     let isValid: boolean;
     let AP = Math.sqrt(x ** 2 + y ** 2)
@@ -64,22 +87,27 @@ function inv_solve(x: number, y: number, z: number): any[] {
     let theta1_1 = Math.atan2(y, x)
     let theta1_2 = theta1_1 + Math.PI
     // convert to degrees
-    theta1_1 = theta1_1 * (180 / Math.PI)
-    theta1_2 = theta1_2 * (180 / Math.PI)
-    theta3_1 = theta3_1 * (180 / Math.PI)
-    theta3_2 = theta3_2 * (180 / Math.PI)
-    theta2_1 = theta2_1 * (180 / Math.PI)
-    theta2_2 = theta2_2 * (180 / Math.PI)
-    theta2_3 = theta2_3 * (180 / Math.PI)
-    theta2_4 = theta2_4 * (180 / Math.PI)
-    theta1_1 = Math.round(theta1_1)
-    theta1_2 = Math.round(theta1_2)
-    theta2_1 = Math.round(theta2_1)
-    theta2_2 = Math.round(theta2_2)
-    theta2_3 = Math.round(theta2_3)
-    theta2_4 = Math.round(theta2_4)
-    theta3_1 = Math.round(theta3_1)
-    theta3_2 = Math.round(theta3_2)
+    /** 
+    theta1_1 = theta1_1 * (180/Math.PI)
+    theta1_2 = theta1_2 * (180/Math.PI)
+
+    theta3_1 = theta3_1 * (180/Math.PI)
+    theta3_2 = theta3_2 * (180/Math.PI)
+    
+    theta2_1 = theta2_1 * (180/Math.PI)
+    theta2_2 = theta2_2 * (180/Math.PI)
+    theta2_3 = theta2_3 * (180/Math.PI)
+    theta2_4 = theta2_4 * (180/Math.PI)
+    
+ */
+    theta1_1 = convertToDegrees(theta1_1)
+    theta1_2 = convertToDegrees(theta1_2)
+    theta2_1 = convertToDegrees(theta2_1)
+    theta2_2 = convertToDegrees(theta2_2)
+    theta2_3 = convertToDegrees(theta2_3)
+    theta2_4 = convertToDegrees(theta2_4)
+    theta3_1 = convertToDegrees(theta3_1)
+    theta3_2 = convertToDegrees(theta3_2)
     let set1 = [theta1_1, theta2_1, theta3_1]
     let set2 = [theta1_1, theta2_2, theta3_2]
     let set3 = [theta1_2, theta2_3, theta3_1]
@@ -107,11 +135,28 @@ function inv_solve(x: number, y: number, z: number): any[] {
         }
         
     }
-    for (let _s of res) {
-        serial.writeValue("theta1", _s[0])
-        serial.writeValue("theta2", _s[1])
-        serial.writeValue("theta3", _s[2])
+    if (res.length == 0) {
+        serial.writeLine("no values work :(")
+    } else {
+        for (let _s of res) {
+            serial.writeValue("theta1", _s[0])
+            serial.writeValue("theta2", _s[1])
+            serial.writeValue("theta3", _s[2])
+        }
     }
+    
     return res
+}
+
+function convertToDegrees(theta: number): number {
+    // checking for nan, nan is not equal to itself
+    if (theta != theta) {
+        return Math.round(theta * (180 / Math.PI))
+    } else {
+        // print("Error: cannot perform operation with NaN value.")
+        // returns -1 because later we get rid of sets that have negative thetas
+        return -1
+    }
+    
 }
 
